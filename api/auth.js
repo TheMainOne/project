b8998bb09d6ceab14ebe74b25da5c24558e7a3f2
Maksym Lvov <83756212+TheMainOne@users.noreply.github.com>
2024-09-02 15:38:54 -0400

    auth logic added

--- api/auth.js
+++ api/auth.js
@@ -1,13 +1,12 @@
 import express from "express";
-import passport from 'passport';
 import controllers from "../controllers/authController.js";
-import "../services/passport/passport.js"
+import authenticate from "../middlewares/authenticate.js";
+import "../services/passport/passport.js";
 
 const authRouter = express.Router();
-const authenticate = passport.authenticate('jwt', { session: false });
 
-authRouter.post('/register', controllers.register);
-authRouter.post('/login', controllers.login);
+authRouter.post("/register", controllers.register);
+authRouter.post("/login", controllers.login);
 authRouter.post("/logout", authenticate, controllers.logout);
 
 export default authRouter;
--- api/material.js
+++ api/material.js
@@ -1,22 +1,35 @@
 import express from "express";
-import passport from 'passport';
+import passport from "passport";
 import controllers from "../controllers/materialsController.js";
 import isValidId from "../middlewares/isValidId.js";
 import validateBody from "../middlewares/validateBody.js";
+import authenticate from "../middlewares/authenticate.js";
 import { materialSchema } from "../services/schemas/material.js";
-
 
 // creating a new router
 const router = express.Router();
-const authenticate = passport.authenticate('jwt', { session: false }); // middleware for checking auth 
 
 // Assigning new paths for the router
 router.get("/api/materials", authenticate, controllers.getAll);
 router.get("/api/materials/:id", authenticate, isValidId, controllers.getById);
-router.post("/api/materials", authenticate, validateBody(materialSchema.validateMaterialSchema), controllers.createMaterial);
-router.put("/api/materials/:id", authenticate, isValidId, validateBody(materialSchema.updateMaterialSchema), controllers.updateByID);
-router.delete("/api/materials/:id", authenticate, isValidId, controllers.deleteMaterial);
-
-
+router.post(
+  "/api/materials",
+  authenticate,
+  validateBody(materialSchema.validateMaterialSchema),
+  controllers.createMaterial
+);
+router.put(
+  "/api/materials/:id",
+  authenticate,
+  isValidId,
+  validateBody(materialSchema.updateMaterialSchema),
+  controllers.updateByID
+);
+router.delete(
+  "/api/materials/:id",
+  authenticate,
+  isValidId,
+  controllers.deleteMaterial
+);
 
 export default router;
