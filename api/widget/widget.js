e355a8baa22773b4c876021f2b506629bc235fce
Maksym Lvov <83756212+TheMainOne@users.noreply.github.com>
2025-09-29 11:42:19 -0400

    Update widget.js

--- api/widget/widget.js
+++ api/widget/widget.js
@@ -156,5 +156,6 @@ router.post("/chat", async (req, res) => {
   }
 });
 router.options("/chat", cors(), (req, res) => res.sendStatus(204));
+router.get("/ping", (req, res) => res.json({ ok: true, t: Date.now() }));
 
 export default router;
