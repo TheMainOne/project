13acbd361d165dc11bb34a1ba6008a878bfba520
Maksym Lvov <83756212+TheMainOne@users.noreply.github.com>
2025-11-03 11:43:47 -0500

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 import User from "../models/user.js";
-import Token from "../models/Token.js";
+import Token from "../models/token.js";
 import { signAccess, signRefresh, verifyRefresh } from "../utils/jwt.js";
 import ms from "ms";
 
