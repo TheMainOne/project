49c0aa3b4abf7c750d325556c468ccdae7e9d742
Maksym Lvov <83756212+TheMainOne@users.noreply.github.com>
2025-12-04 22:01:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -114,6 +114,7 @@ mongoose
     app.use("/api/statistic", aiwStatsRouter);
 
 const aiwRouter = express.Router();
+aiwRouter.use("/auth", authRouter);  // теперь будет /api/aiw/auth/...
 
 aiwRouter.use(chatRouter);     // /chat
 aiwRouter.use(retrieveRouter); // /search, ...
